e00c464c4c706b6f752a7388d92425b27343bd27
Robert Paczkowski <rpaczkowski98@gmail.com>
2019-07-04 23:22:48 +0200

    First Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla;
+
+public class Calculator {
+    public void adding(int a, int b){
+        System.out.println(a+b);
+    }
+    public void substraction(int a, int b){
+        System.out.println(a-b);
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.adding(14, 16);
+        calculator.substraction(32, 14);
+    }
+}
